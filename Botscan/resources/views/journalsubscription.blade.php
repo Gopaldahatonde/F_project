636cc8aa9eb0818b1854b2584980568548788e56
gopal dahatonde <dahatondegopal227@gmail.com>
2024-02-29 18:49:19 +0530

    new project

---
+++ Botscan/resources/views/journalsubscription.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.header')
+@section('webcontent')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Journal Subscriptions</title>
+    <link rel="stylesheet" href="css/journalS.css">
+</head>
+
+<body class="body_css">
+
+
+
+    <header class="header2">
+        <h3><b>Journal Subscriptions</b></h3>
+    </header>
+
+    <section class="section2">
+        <h4>About Us</h4>
+        <p>BotScanis provides the best journal subscription services in various subjects and specializations in animal sciences. Our experienced and professional team efficiently handles subscription work. Subscribers are provided with an online tracking and notification system to track the dispatched publications.</p>
+        <p>Subscriptions are delivered through courier and registered post.</p>
+
+        <h4>Subscribe Now</h4>
+        <p>If you're interested in staying updated with the latest research in animal sciences, subscribe to our journals now!</p>
+        <a href="#" class="subscribe-btn2">Subscribe</a>
+    </section>
+
+
+</body>
+
+</html>
+
+
+@endsection
